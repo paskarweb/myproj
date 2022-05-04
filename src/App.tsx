dfe641f1717d9ff9644363df2a9c56d1966e1c9f
paskarweb <58509133+paskarweb@users.noreply.github.com>
2022-05-04 15:24:49 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import './App.css';
 import { Footer } from './Footer';
 
 
-const user = { userName: 'User1' }
+const user = {
+  userName: 'User1',
+  FirstName: '',
+  ShortBiography: '',
+  PublicContacts: ''
+}
 const App = () => (
   <div className="App">
     <h1>Hello world - {user.userName}</h1>
+    <h2>First Name: {user.FirstName}</h2>
+    <h2>Short biography: {user.ShortBiography}</h2>
+    <h2>Public contacts: {user.PublicContacts}</h2>
     <Footer copyright='C 57' />
   </div>
 )
