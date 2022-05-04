ec9f8e6100d768e940b0b1d27178e84eac3cc6ac
paskarweb <58509133+paskarweb@users.noreply.github.com>
2022-05-04 16:42:57 +0300

    DZ 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import './App.css';
 import { Footer } from './Footer';
+import UsrData from "./data/userdata.json";
 
 const user = {
   userName: 'User1',
-  FirstName: 'Ivan',
-  ShortBiography: 'Back-End Developer. C#, Delphi, MSSQL, Oracle',
-  PublicContacts: ''
 }
 
+//console.log(UsrData.FirstName);
+
 const App = () => (
-  <div className="App">
-    <h1>Hello world - {user.userName}</h1>
-    <h2>First Name: {user.FirstName}</h2>
-    <h2>Short biography: {user.ShortBiography}</h2>
-    <h2>Public contacts: {user.PublicContacts}</h2>
-    <Footer copyright='C 57' />
+  <div className="App-header">
+    <h1>Hello user!</h1>
+    <div>
+      <ul>
+        <li>First Name: {UsrData.users.FirstName}</li>
+        <li>Short biography: {UsrData.users.ShortBiography}</li>
+        <li>Public contacts: {UsrData.users.PublicContacts}</li>
+      </ul>
+    </div>
+    <Footer copyright='copyright 01.05.2022' />
   </div>
 )
 
